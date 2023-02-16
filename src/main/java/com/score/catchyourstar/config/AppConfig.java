86830dbf7c94003cb383d900432c4d52a1344c05
parkjiyun <pjyun05200@gmail.com>
2023-02-16 21:46:52 +0900

    add app config

---
+++ src/main/java/com/score/catchyourstar/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.score.catchyourstar.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://catch-your-star.netlify.app");
+    }
+
+}
